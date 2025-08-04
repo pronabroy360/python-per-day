45a8409404bae23c3f077529fbd3adb780b2c0e0
pronabroy360 <60190011+pronabroy360@users.noreply.github.com>
2025-08-04 13:21:24 -0700

    Update 2. Without repeating characters_brute_force.py

--- Sliding window and two pointers/2. Without repeating characters_brute_force.py
+++ Sliding window and two pointers/2. Without repeating characters_brute_force.py
@@ -1,15 +1,17 @@
-def repeating_without_characters (s : str):
-    l = 0 
-    r = 0 
-    maxlen = 0 
-    setx = set()
-    for r in range(len(s)):
-        while s[r] in setx:
-            setx.remove(s[l])
-            l+= 1 
-        setx.add(s[r])
-        maxlen = max(maxlen, r - l + 1)
+def withoutRepeatingCharacters(s: str):
+    if len(s) == 0:
+        return 0 
+    maxlen = 0
+
+    for i in range(len(s)):
+        setx = {} # optionally can use setx = set() here. Changes in line no 9 and 11 would also be there
+        for j in range(i, len(s)):          # fix here: explore substrings starting at i
+            if s[j] in setx:
+                break
+            setx[s[j]] = 1                  #setx.add(s[j])
+            maxlen = max(maxlen, j - i + 1)  # fix: calculate substring length
+
     return maxlen
-    
-s = "abcabdaecbd"
-print(repeating_without_characters(s))
+
+s = "aabcabcdebc"
+print(withoutRepeatingCharacters(s))  # Output: 5 ("abcde")
