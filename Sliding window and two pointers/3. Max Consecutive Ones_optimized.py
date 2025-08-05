1705f98261d4012544566669709e66432158df97
pronabroy360 <60190011+pronabroy360@users.noreply.github.com>
2025-08-05 10:50:48 -0700

    Create 3. Max Consecutive Ones_optimized.py

---
+++ Sliding window and two pointers/3. Max Consecutive Ones_optimized.py
@@ -0,0 +1,22 @@
+def flipZero(arr, k):
+    count_of_zero = 0 
+    max_len = 0
+    r = 0
+    l = 0
+    
+    for r in range(len(arr)):
+        if arr[r] == 0:
+            count_of_zero += 1 
+        
+        while count_of_zero > k:
+            if arr[l] == 0:
+               count_of_zero -= 1 
+             
+            l += 1
+        
+        max_len = max(max_len, r - l + 1)
+    return max_len
+
+if __name__ == "__main__":
+    array = [1, 1, 1, 0, 0, 1, 1, 0]
+    print(flipZero(array, 2))
