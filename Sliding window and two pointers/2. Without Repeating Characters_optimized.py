b7145fffd1de35f8bc296c4fd7dbb98f7a78b3dc
pronabroy360 <60190011+pronabroy360@users.noreply.github.com>
2025-08-05 15:23:20 -0700

    Update 2. Without Repeating Characters_optimized.py

--- Sliding window and two pointers/2. Without Repeating Characters_optimized.py
+++ Sliding window and two pointers/2. Without Repeating Characters_optimized.py
@@ -1,17 +1,23 @@
-def withoutRepeatingCharacters(s: str):
+def without_replacing_characters(s: str):
+    l = 0
+    r = 0 
+    max_len = 0
+    
     if len(s) == 0:
-        return 0 
-    maxlen = 0
+        return 0
+    
+    setx = set()
+    
+    for r in range(len(s)):
+        if s[r] in setx:
+            setx.remove(s[r])
+            l += 1 
+        setx.add(s[r])
+        r += 1
+        
+        max_len = max(max_len, r - l)
+    return max_len
 
-    for i in range(len(s)):
-        setx = {}
-        for j in range(i, len(s)):  # fix here: explore substrings starting at i
-            if s[j] in setx:
-                break
-            setx[s[j]] = 1
-            maxlen = max(maxlen, j - i + 1)  # fix: calculate substring length
-
-    return maxlen
-
-s = "aabcabcdebc"
-print(withoutRepeatingCharacters(s))
+if __name__ == "__main__":
+    s = "abcabdcef"
+    print(without_replacing_characters(s))
