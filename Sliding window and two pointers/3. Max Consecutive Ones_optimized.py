1d07b11bd8a927fac5c013791e68d1a5ff501d36
pronabroy360 <60190011+pronabroy360@users.noreply.github.com>
2025-08-12 16:40:56 -0700

    Update 3. Max Consecutive Ones_optimized.py

--- Sliding window and two pointers/3. Max Consecutive Ones_optimized.py
+++ Sliding window and two pointers/3. Max Consecutive Ones_optimized.py
@@ -12,7 +12,8 @@ def flipZero(arr, k):
             if arr[l] == 0:
                count_of_zero -= 1 
              
-            l += 1                             # I once mistakenly put this l value within if loop. the answer comes fine as 1 less - result 8 coming as 7.
+            l += 1                             # I once mistakenly put this l value within if loop. the answer comes fine as 1 less - result 8 coming as 7. you only move l forward when arr[l] == 0, which means if you encounter a 1 at l, the loop can get stuck.
+                                               # For example, if arr[l] is 1, the code will never increase l in that loop, causing an infinite loop for certain inputs.
         
         max_len = max(max_len, r - l + 1)
     return max_len
